1700d6d9937a0e496476dc7e4a008b62972cf1ff
Leheng620 <llh238704688@gmail.com>
2021-02-05 21:22:00 -0500

    published

---
+++ precache-manifest.a99f6707582ea0b5fdd71153bda0cbcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02170bed74bbcdaf8d314c2622a9c54e",
+    "url": "/todo-list-no-database/index.html"
+  },
+  {
+    "revision": "855b8e411c233fee252f",
+    "url": "/todo-list-no-database/static/css/main.94235bee.chunk.css"
+  },
+  {
+    "revision": "67da3bc0dfb295491696",
+    "url": "/todo-list-no-database/static/js/2.4f7f0ed2.chunk.js"
+  },
+  {
+    "revision": "855b8e411c233fee252f",
+    "url": "/todo-list-no-database/static/js/main.f6821204.chunk.js"
+  },
+  {
+    "revision": "5b4607782c72ddb14d11",
+    "url": "/todo-list-no-database/static/js/runtime-main.126d42ab.js"
+  }
+]);
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/todo_layout.css';
-import './css/todo_style.css';
-import App from './App';
-import './jtps/jTPS';
-import './jtps/jTPS_Transaction';
-
-ReactDOM.render(<App />, document.getElementById('root'));
